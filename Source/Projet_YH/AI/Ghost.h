3c1ef88589ad3f55bcf106c8420aad40f1f24a98
Hinenao <hinenaolamberty@outlook.fr>
2025-12-03 16:18:58 +0100

    see description
    -ghost subclass
    -function added to parent ai in order to make it to react when seen by player
    -minor update to tree and blackboard
    -BP ai for testing

---
+++ Source/Projet_YH/AI/Ghost.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../AI/AI_Base.h"
+#include "Ghost.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_YH_API AGhost : public AAI_Base
+{
+	GENERATED_BODY()
+	
+};
